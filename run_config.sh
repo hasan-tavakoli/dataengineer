aa543d7f89e048ad950b799b69f336fd01058da8
Hassan Tavakoli <51287865+hasan-tavakoli@users.noreply.github.com>
2022-06-12 02:11:11 +0430

    add config

--- run_config.sh
+++ run_config.sh
@@ -1,3 +1,3 @@
 docker run --rm "debian:bullseye-slim" bash -c 'numfmt --to iec $(echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE))))'
 echo -e "AIRFLOW_UID=$(id -u)" > .env
-
+mkdir -p  ./logs ./plugins
